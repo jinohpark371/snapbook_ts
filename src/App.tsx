30b219a8bf0450eea76a7cd38c60a71052562c7a
jinohpark371 <jinoh1030@naver.com>
2026-02-12 16:56:27 +0900

    feat: 로그인 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+﻿import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import AuthRedirectPage from './pages/AuthRedirectPage';
 import GlobalStyle from './styles/GlobalStyled';
 import { AuthProvider } from './context/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 const queryClient = new QueryClient();
 
 function App() {
@@ -32,6 +34,7 @@ function AppRoutes() {
     <Routes>
       {/* 로그인 여부에 따라 분기 */}
       <Route path="/" element={auth ? <HomePage /> : <LoginPage />} />
+      <Route path="/auth" element={<AuthRedirectPage />} />
     </Routes>
   );
 }
