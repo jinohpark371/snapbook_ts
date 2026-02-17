e4448fbb692b2126140e61b41029771039823b09
jinohpark371 <jinoh1030@naver.com>
2026-02-17 16:23:27 +0900

    fix: SubmitButton min-height 속성 추가

--- src/components/submit/SubmitButton.tsx
+++ src/components/submit/SubmitButton.tsx
@@ -5,6 +5,7 @@ import theme from '../../styles/theme';
 export const SubmitButton = styled(BaseButton)`
   width: ${({ $width }) => $width || '305px'};
   height: ${({ $height }) => $height || '55px'};
+  min-height: ${({ $height }) => $height || '55px'};
   font-size: ${({ $fontSize }) => $fontSize || '18px'};
 
   background-color: ${theme.colors.primary};
