1d58726df614af55a3237950ce8c810352fe3099
jinohpark371 <jinoh1030@naver.com>
2026-02-17 15:59:37 +0900

    feat: 회원가입 고객, 점주 분기에 따른 페이지 라우팅 구성

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useAuth } from './context/AuthContext';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import AuthRedirectPage from './pages/AuthRedirectPage';
+import SignupGatePage from './pages/SignupGatePage';
+import SignupPage from './pages/SignupPage';
 import GlobalStyle from './styles/GlobalStyled';
 import { AuthProvider } from './context/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -35,6 +37,9 @@ function AppRoutes() {
       {/* 로그인 여부에 따라 분기 */}
       <Route path="/" element={auth ? <HomePage /> : <LoginPage />} />
       <Route path="/auth" element={<AuthRedirectPage />} />
+      <Route path="/signup" element={<SignupGatePage />} />
+      <Route path="/signup/customer" element={<SignupPage userType="CUSTOMER" />} />
+      <Route path="/signup/owner" element={<SignupPage userType="OWNER" />} />
     </Routes>
   );
 }
